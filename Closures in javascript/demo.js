12f45fc024d2feb2812dca6dff9a0dca759dd080
Rohit Vishwakarma <rohit8840@gmail.com>
2024-02-02 09:55:13 +0530

    adding closures

--- Closures in javascript/demo.js
+++ Closures in javascript/demo.js
@@ -1,9 +1,10 @@
 function todo(task){
-
+console.log("start of todo ");
     setTimeout(function fun(){
         console.log("completed ",task);
     },2000);
 
+    console.log("end of todo ");
 
 }
 
